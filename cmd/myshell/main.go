40ac3d9e8df3bfb3f6d699980b7d153d68d67477
Chi Wei Leong <chiwei0708@gmail.com>
2025-02-06 18:43:22 +0800

    exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type CmdFn = func([]string) (string, error)
+
+var builtinCmd = map[string]CmdFn{}
+
 func main() {
+	initCommands()
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -17,6 +24,45 @@ func main() {
 		}
 
 		input = strings.TrimSpace(input)
-		fmt.Printf("%s: command not found\n", input)
+		args := strings.Fields(input)
+		if len(args) == 0 {
+			fmt.Println("Please provide a command")
+			continue
+		}
+
+		command := args[0]
+		args = args[1:]
+		cmdFn, exist := builtinCmd[command]
+		if exist {
+			cmdFn(args)
+		} else {
+			fmt.Printf("%s: command not found\n", input)
+		}
 	}
+}
+
+func initCommands() {
+	registerCmd("exit", exitCmd)
+}
+
+func registerCmd(key string, cmdFn CmdFn) {
+	builtinCmd[key] = cmdFn
+}
+
+func exitCmd(args []string) (string, error) {
+	if len(args) == 0 {
+		return "", errors.New("please provide a status code for the exit command")
+	}
+
+	if len(args) > 1 {
+		return "", errors.New("too many argument provided for the exit command")
+	}
+
+	code, err := strconv.Atoi(args[0])
+	if err != nil {
+		return "", err
+	}
+
+	os.Exit(code)
+	return "", nil
 }
