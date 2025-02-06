a1b4efa524c5811ab261c3ad770d6f5a70d998fb
Chi Wei Leong <chiwei0708@gmail.com>
2025-02-06 18:03:02 +0800

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,13 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("Error parsing user input: %s", err.Error())
-		os.Exit(1)
-	}
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error parsing user input: %s", err.Error())
+			os.Exit(1)
+		}
 
-	input = strings.TrimSpace(input)
-	fmt.Printf("%s: command not found\n", input)
+		input = strings.TrimSpace(input)
+		fmt.Printf("%s: command not found\n", input)
+	}
 }
